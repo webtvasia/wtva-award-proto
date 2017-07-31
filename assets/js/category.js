a6a3641e9f3a5e0c5c6726e4c9c3751c05c9cb20
Rezkya Tinnov <rezkyatinnov@gmail.com>
2017-07-31 15:56:11 +0800

    removes radius for images on social feed

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,14 +1,15 @@
 $(window).on('load', function () {
-	$('#loading').delay(0).fadeOut(1000, function () {
+    $('#loading').delay(0).fadeOut(1000, function () {
 
-	});
+    });
 });
 
-$(document).ready(function () {
-    $('#myCarousel').carousel({
-        interval: 1000
-    })
-    $('.fdi-Carousel .item').each(function () {
+(function () {
+    $('#carousel123').carousel({
+        interval: 2000
+    });
+
+    $('.carousel-showsixmoveone .item').each(function () {
         var next = $(this).next();
         if (!next.length) {
             next = $(this).siblings(':first');
@@ -17,31 +18,23 @@ $(document).ready(function () {
 
         if (next.next().length > 0) {
             next.next().children(':first-child').clone().appendTo($(this));
-        }
-        else {
+        } else {
             $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
         }
+        // var itemToClone = $(this);
+
+        // for (var i=1;i<3;i++) {
+        //   itemToClone = itemToClone.next();
+
+        //   // wrap around if at end of item collection
+        //   if (!itemToClone.length) {
+        //     itemToClone = $(this).siblings(':first');
+        //   }
+
+        //   // grab item, clone, add marker class, add to collection
+        //   itemToClone.children(':first-child').clone()
+        //     .addClass("cloneditem-"+(i))
+        //     .appendTo($(this));
+        // }
     });
-});
-
-(function(){
-  $('#carousel123').carousel({ interval: 2000 });
-  
-  $('.carousel-showsixmoveone .item').each(function(){
-    var itemToClone = $(this);
-
-    for (var i=1;i<3;i++) {
-      itemToClone = itemToClone.next();
-
-      // wrap around if at end of item collection
-      if (!itemToClone.length) {
-        itemToClone = $(this).siblings(':first');
-      }
-
-      // grab item, clone, add marker class, add to collection
-      itemToClone.children(':first-child').clone()
-        .addClass("cloneditem-"+(i))
-        .appendTo($(this));
-    }
-  });
 }());
