4575008bef0bf267d6c5d9faeeb390e84a0aa686
Rezkya Tinnov <rezkyatinnov@gmail.com>
2017-07-31 14:58:08 +0800

    category and social feed

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,6 +1,47 @@
+$(window).on('load', function () {
+	$('#loading').delay(0).fadeOut(1000, function () {
 
-	$(window).on('load', function(){
-		$('#loading').delay(0).fadeOut(1000,function(){
-
-        });	
 	});
+});
+
+$(document).ready(function () {
+    $('#myCarousel').carousel({
+        interval: 1000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
+
+(function(){
+  $('#carousel123').carousel({ interval: 2000 });
+  
+  $('.carousel-showsixmoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<3;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
